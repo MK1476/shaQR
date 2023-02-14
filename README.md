804e06a51396102d7b179496a5c36ab8461b7610
MK1476 <99121asif@gmail.com>
2023-02-14 21:51:30 +0530

    readme edit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # shaQR
-Share through QR helps to share data between devices using QR code
+Generates QR codes from selected text or links in Chrome.
+
+## Installation
+1. Clone or download this repository.
+2. In Chrome, navigate to chrome://extensions.
+3. Enable "Developer mode" in the top right corner.
+4. Click "Load unpacked" and select the directory where you cloned/downloaded this repository.
+## Usage
+To generate a QR code for selected text or a link, do the following:
+
+* Right-click on the selected text or link.
+* Click the "share with shaQR " option in the context menu.
+* A popup window will open with the generated QR code.
+## Customization
+You can customize the QR code size by modifying the width and height properties in the popup.js file.
+
+## Credits
+This extension uses the qrcode library (version 1.4.4) from https://github.com/davidshimjs/qrcodejs.
+
+## License
+This project is licensed under the [Creative Commons Zero v1.0 Universal](/LICENSE).
--- popup.js
+++ popup.js
@@ -1,10 +1,3 @@
-import io from 'socket.io-client';
-
-  const socket = io('http://localhost:3000');
-  
-  socket.on('connect', () => {
-    console.log('Connected to the server');
-  });
 
 chrome.runtime.sendMessage({ action: "getQRData" }, function(url) {
     console.log(url + 'jhguhv');
@@ -13,25 +6,3 @@ chrome.runtime.sendMessage({ action: "getQRData" }, function(url) {
     document.getElementById("qr").innerHTML = "<img src="+ url+ "  alt=\"HTML tutorial\" style=\"width:200px;height:200px;\">";
     
 });
-const chatBox = document.getElementById("chat-box");
-const messageInput = document.getElementById("message-input");
-const sendButton = document.getElementById("send-button");
-document.addEventListener('DOMContentLoaded', function () {
-sendButton.addEventListener("click", function() {
-  const message = messageInput.value;
-  chrome.runtime.sendMessage({ type: "sendMessage", message: message });
-  messageInput.value = "";
-});
-
-
-});
-
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.type === "receiveMessage") {
-      const message = request.message;
-      chatBox.innerHTML += message + "<br>";
-    }
-  }
-);
-
